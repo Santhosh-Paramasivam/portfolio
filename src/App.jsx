a6f165514918b90176a1c5ba93357dfb086138c2
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-03 17:05:42 +0530

    Moved skills page title to after introduction page

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import ProjectsPage from './Pages/Projects'
 import SkillsPage from './Pages/SkillsPage'
 
 function App() {
-    return <div className='d-flex flex-column'>
+    return <div className='d-flex flex-column' style={{backgroundColor: "#15102E"}}>
         <Introduction />
-        <ProjectsPage />
+        <div style={{ backgroundColor: '#6F66FD', padding: 0.5, marginBottom: 10}}/>
         <SkillsPage />
+        <ProjectsPage />
     </div>
 }
 export default App
--- src/Pages/SkillsPage.jsx
+++ src/Pages/SkillsPage.jsx
@@ -3,7 +3,7 @@ import Frameworks from './Skills/Frameworks'
 import Languages from './Skills/Languages'
 
 function SkillsPage() {
-    return <div className="d-flex flex-column pt-3 pb-4" style={{ backgroundColor: "#15102E" }}>
+    return <div className="d-flex flex-column pt-0 mt-0 pb-4" style={{ backgroundColor: "#15102E" }}>
         <h2 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center" >Skills</h2>
         <h3 style={{ fontFamily: 'imprima', color: 'white'}} className="mx-2 py-0 my-0 fs-3 px-5 align-self-center">Languages</h3>
         <Languages />
