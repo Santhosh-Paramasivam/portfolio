959649184865891ff31fff52f82a464460e33999
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-03 16:28:33 +0530

    Added skills page title

--- src/Pages/SkillsPage.jsx
+++ src/Pages/SkillsPage.jsx
@@ -4,6 +4,7 @@ import Languages from './Skills/Languages'
 
 function SkillsPage() {
     return <div className="d-flex flex-column pt-3 pb-4" style={{ backgroundColor: "#15102E" }}>
+        <h2 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center" >Skills</h2>
         <h3 style={{ fontFamily: 'imprima', color: 'white'}} className="mx-2 py-0 my-0 fs-3 px-5 align-self-center">Languages</h3>
         <Languages />
         <h3 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 my-0 fs-3 px-5 align-self-center mt-3">Frameworks</h3>
