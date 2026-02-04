2466ea02d76ebf7032755a28341d066f651b7677
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-04 19:53:56 +0530

    Added project section subheadings

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,7 +2,10 @@ import Projects from "./Projects/Projects";
 
 function ProjectsPage() {
     return <div className="d-flex flex-column pt-4 pb-5" style={{ backgroundColor: "#211A43" }}>
-        <h3 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 my-0 fs-1 px-5 align-self-center">Projects</h3>
+        <h3 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center">Projects</h3>
+        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-0 mt-2 fs-3 px-5 align-self-center">Featured</h4>
+        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-0 mt-2 fs-3 px-5 align-self-center">Fun</h4>
+        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-0 mt-2 fs-3 px-5 align-self-center">Internet of Things</h4>
         <Projects />
     </div>
 }
