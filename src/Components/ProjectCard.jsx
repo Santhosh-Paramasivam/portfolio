20b8d810729b7f681dcf75b07f2d945e212f15d5
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-04 22:59:26 +0530

    Make project cards less bright, and the text more readable

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -4,8 +4,8 @@ function ProjectCard({ title, image, classes, tools, description, img_height, ca
     return <div className={classes ? "card project-card mb-2" + classes : "card project-card mb-3"} style={{ width: "100%", maxWidth: 400, minWidth:300 }}>
         <img className="card-img-top" src={`static/` + image} />
         <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p>{description}</p>
+            <h5 className="card-title" style={{ color: 'white' }}>{title}</h5>
+            <p style={{ color: 'white' }}>{description}</p>
             <a href={"https://github.com/Santhosh-Paramasivam/" + link} className='link-light link-underline-opacity-0'>
                 <img src={`static/github.png`} alt="GitHub Icon" width="20px" className='icon mx-2 mb-1' />
                 Github
@@ -14,7 +14,7 @@ function ProjectCard({ title, image, classes, tools, description, img_height, ca
 
         {tools && <div className='card-footer d-flex flex-row justify-content-end g-5 px-1'>
             {tools.map((value) => {
-                return <h6 key={value} className='my-0 px-1'><span className='badge bg-white text-dark rounded-1 p-2'>{value}</span></h6>
+                return <h6 key={value} className='my-0 px-1'><span className='badge rounded-2 p-1 technologies-used'>{value}</span></h6>
             })}
         </div>}
     </div>
