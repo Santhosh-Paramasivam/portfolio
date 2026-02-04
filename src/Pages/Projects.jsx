cfc97db3eedeff38b1ac2ec1f8f51017c7ac918d
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-04 23:16:22 +0530

    Made the project subsections more readable

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,15 +1,16 @@
 import FeaturedProjects from "./Projects/FeaturedProjects";
 import FunProjects from "./Projects/FunProjects";
 import IOTProjects from "./Projects/IOTProjects";
+import "./Projects/Projects.css";
 
 function ProjectsPage() {
     return <div className="d-flex flex-column pt-2 pb-5" style={{ backgroundColor: "#15102E" }}>
         <h3 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center">Projects</h3>
-        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Featured</h4>
+        <h4 className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center subsection-title">My Best Projects</h4>
         <FeaturedProjects/>
-        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Fun</h4>
+        <h4 className="mx-2 py-0 pt-2 mb-3 mt-2 fs-3 px-5 align-self-center subsection-title">Fun Projects</h4>
         <FunProjects/>
-        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Internet of Things</h4>
+        <h4 className="mx-2 py-0 pt-2 mb-3 mt-2 fs-3 px-5 align-self-center subsection-title">Internet of Things Projects</h4>
         <IOTProjects/>
     </div>
 }
