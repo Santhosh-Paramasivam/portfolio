ff6041881379dd3fd47c2f891b8cdd7277716e2f
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-04 22:41:47 +0530

    Make project page bg colour same as the rest of the webpage

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -3,7 +3,7 @@ import FunProjects from "./Projects/FunProjects";
 import IOTProjects from "./Projects/IOTProjects";
 
 function ProjectsPage() {
-    return <div className="d-flex flex-column pt-4 pb-5" style={{ backgroundColor: "#211A43" }}>
+    return <div className="d-flex flex-column pt-2 pb-5" style={{ backgroundColor: "#15102E" }}>
         <h3 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center">Projects</h3>
         <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Featured</h4>
         <FeaturedProjects/>
