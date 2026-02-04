fb7144c97a7f1c2265bcdc780d8c3e743c169124
Santhosh-Paramasivam <santhoshparamasivam78@gmail.com>
2026-02-04 21:52:52 +0530

    Successfully extracted the fun section into its own top-level section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,4 +1,5 @@
 import FeaturedProjects from "./Projects/FeaturedProjects";
+import FunProjects from "./Projects/FunProjects";
 import Projects from "./Projects/Projects";
 
 function ProjectsPage() {
@@ -6,7 +7,8 @@ function ProjectsPage() {
         <h3 style={{ fontFamily: 'imprima', color: '#96DEFF' }} className="mx-2 py-0 mt-0 mb-2 fs-1 px-5 align-self-center">Projects</h3>
         <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Featured</h4>
         <FeaturedProjects/>
-        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-0 mt-2 fs-3 px-5 align-self-center">Fun</h4>
+        <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-3 mt-2 fs-3 px-5 align-self-center">Fun</h4>
+        <FunProjects/>
         <h4 style={{ fontFamily: 'imprima', color: 'white' }} className="mx-2 py-0 mb-0 mt-2 fs-3 px-5 align-self-center">Internet of Things</h4>
         <Projects />
     </div>
